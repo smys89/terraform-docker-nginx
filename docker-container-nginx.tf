c682952813ec91df5fd884ad63742cde1e1ea160
smys89 <smys89@gmail.com>
2025-03-16 15:47:53 +0400

    Adding docker provider, NGINX container and variables

--- docker-container-nginx.tf
+++ docker-container-nginx.tf
@@ -3,12 +3,20 @@ resource "docker_container" "nginx" {
   name  = "nginx"
   image = docker_image.nginx.image_id
   ports {
-    internal = "80"
-    external = "80"
+    internal = var.port_no
+    external = var.port_no
   }
 }
 
 # Find the latest NGINX image.
 resource "docker_image" "nginx" {
-  name = "nginx:latest"
+  name = var.nginx_image
+}
+
+variable "port_no" {
+    type = string
+}
+
+variable "nginx_image" {
+    type = string
 }
---
+++ variables.tf
@@ -0,0 +1,2 @@
+port_no      = "80"
+nginx_image  = "nginx:latest"
